0ff5256cf2b1ad1f104a04b8b15c521679fd746a
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2020-11-26 00:20:42 -0300

    Front-end com ReactJS (Day 4)
    - Estado e Imutabilidade
    - Importando CSS e imagens
    - Listando Projetos da API
    - Cadastrando Projetos

--- code/src/App.js
+++ code/src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 import Menu from './components/Menu';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects(['Project 1', 'Project 2']);
+  }, []);
+
+  function handleAddProject() {
+    setProjects([...projects, `New project ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage" />
+      <Header title="Projects" />
+
+      <img width={300} src={backgroundImage} alt="background image"/>
+      
       <Menu>
-        <ul>Homepage</ul>
-        <ul>Projects</ul>
-        <ul>Login</ul>
+        <ul>
+          {projects.map(project => <li key={project}>{project}</li>)}
+        </ul>
+
+        <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
       </Menu>
     </>
   );
